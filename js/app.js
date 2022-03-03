d25d2ca40da45342dfd2354ad4f55f9b0856fc9f
Basirul Billah <basirulbillah@gmail.com>
2022-03-03 21:58:55 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,22 @@
+const API_KEY = `1081a9e2e21dbf6637eb935f23f8a74f`;
+const loadWeather = () => {
+    const cityName = document.getElementById('input-field').value;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`;
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayData(data));
+}
+
+const setInnerText = (id, text) => {
+    document.getElementById(id).innerText = text;
+}
+
+const displayData = (data) => {
+    setInnerText('city-name', data.name);
+    setInnerText('temperature', data.main.temp);
+    setInnerText('condition', data.weather[0].main);
+    
+    // set weather icon 
+    const url = 
+}
